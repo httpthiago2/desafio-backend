d1e64d39ffe023ae6c0953799e861961b63bc4ec
Thiago Gonzalez <thiagogonzalez.dev@gmail.com>
2023-11-15 18:35:15 -0300

    initial project files

---
+++ src/main/java/com/httpthiago/desafiostonepayments/DesafioStonePaymentsApplication.java
@@ -0,0 +1,13 @@
+package com.httpthiago.desafiostonepayments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioStonePaymentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioStonePaymentsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/httpthiago/desafiostonepayments/DesafioStonePaymentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.httpthiago.desafiostonepayments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioStonePaymentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
